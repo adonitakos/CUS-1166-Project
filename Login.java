f5b25047ac7ddd6ef24106706a14578cfaf51bfa
Teuta <teutaelezaj101@gmail.com>
2023-02-18 14:52:20 -0500

    Modifying Login.java so that username + password is not hard-coded, there are rules to the username + pass, and this information is written to the Credentials.txt file. The credentials in this text file are saved even when the program terminates.

--- Login.java
+++ Login.java
@@ -2,7 +2,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.lang.Exception;
+import java.util.Scanner;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 
 class CreateLoginForm extends JFrame implements ActionListener {
 // Initializing Variables
@@ -41,22 +46,62 @@ class CreateLoginForm extends JFrame implements ActionListener {
     } // <--- CreateLoginForm() constructor ends here
 
 // Action listener method for the submit button
-    public void actionPerformed(ActionEvent ae) {
-    // Assigning the information that will be inputted by the user as string variables
-        String userValue = userField.getText();
-        String passValue = passwordField.getText();
 
-    // ! --- [Temporary solution to what will later be Firebase Authentication] --- !
-    // IF the user inputs the username and password as 1, it will bring the user to the optionPage 
-        if (userValue.equals("1") && passValue.equals("1")) {
-            optionPage page = new optionPage();
-            page.setVisible(true);
-        }
-    // ELSE, it will tell them it is the wrong user and pass and it will have them try again
-        else {
-            System.out.println("Wrong username and Password!");
-        }
-    } // <--- actionPerformed() method ends here
+public void actionPerformed(ActionEvent ae) {
+    // Assigning the information that will be inputted by the user as string variables
+    String userValue = userField.getText();
+    String passValue = passwordField.getText();
+
+    // validating username and password based on rules - username must be greater than 5, and password must be at least 5 characters and contain one special character.
+    if (userValue.length() <= 5) {
+        System.out.println("ERROR: Username must be greater than 5 characters long.");
+        return;
+    }
+    if (!passValue.matches("^(?=.*[!@#$%^&*(),.?\":{}|<>]).{5,}$")) {
+        System.out.println("ERROR: Password must be at least 5 characters long and contain at least one special character.");
+        return;
+    }
+
+    // write the user-provided credentials to a file
+    try {
+        FileWriter writer = new FileWriter("/Users/teutaelazaj/Desktop/1166/project/Vehicle-Vortex/Credentials.txt", true);
+
+        
+        // may need to adjust file path, can be replaced with this instead
+        // File credentialsFile = new File("Credentials.txt");
+        // FileWriter writer = new FileWriter(credentialsFile);
+        
+        writer.write(userValue + ":" + passValue + System.lineSeparator()); // add newline character
+        writer.close();
+        System.out.println("Credentials successfully saved to file!");
+
+        // Show the option page if credentials are in line with rules
+        optionPage page = new optionPage();
+        page.setVisible(true);
+    } catch (IOException e) {
+        System.out.println("Error writing credentials to file.");
+    }
+} // <--- actionPerformed() method ends here
+
+
+
+
+    // public void actionPerformed(ActionEvent ae) {
+    // // Assigning the information that will be inputted by the user as string variables
+    //     String userValue = userField.getText();
+    //     String passValue = passwordField.getText();
+
+    // // ! --- [Temporary solution to what will later be Firebase Authentication] --- !
+    // // IF the user inputs the username and password as 1, it will bring the user to the optionPage 
+    //     if (userValue.equals("1") && passValue.equals("1")) {
+    //         optionPage page = new optionPage();
+    //         page.setVisible(true);
+    //     }
+    // // ELSE, it will tell them it is the wrong user and pass and it will have them try again
+    //     else {
+    //         System.out.println("Wrong username and Password!");
+    //     }
+    // } // <--- actionPerformed() method ends here
 
 } // <--- CreateLoginForm{} class ends here
 
