d688160c01a86973b5dbd2c289b22ed9657e0d25
Mohammed Tahir Buksh <88285836+Nexosfarius@users.noreply.github.com>
2023-03-13 15:46:04 -0400

    Create VCC.java

---
+++ VCC.java
@@ -0,0 +1,64 @@
+import java.util.List;
+
+public class VCC {
+
+	public VCC() {
+	}
+
+	public VCCDAO(Connection conn) {
+		
+	}
+
+	public List<Cars> getAllCars() {
+
+	}
+
+	public List<Jobs> getAllJobs() {
+
+	}
+
+	public List<Jobs> getCompleteJobs() {
+
+	}
+
+	public List<Jobs> getIncompleteJobs() {
+
+	}
+
+	public Car getCarById(int carId) {
+
+	}
+
+	public Job getJobById(int jobId) {
+
+	}
+
+	public Boolean assignJobs(int jobId, int carId) {
+
+	}
+
+	public int getRedundancy(int jobId) {
+
+	}
+
+	public void setRedundancy(int jobId, int redundancy) {
+
+	}
+
+	public Boolean deleteCar(int carId) {
+
+	}
+
+	public Boolean deleteJob(int jobId) {
+
+	}
+
+	public Boolean transferJobsBetweenCars(int jobID, int originalCarID, int newCarID) {
+
+	}
+
+	public Boolean transferCompletedJobs(int jobId) {
+
+	}
+
+}
