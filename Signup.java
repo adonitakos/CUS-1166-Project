d85f0f4ac05eea0c755c6404eeed5e95c387f18c
Teuta Elezaj <76460786+teutaelezaj@users.noreply.github.com>
2023-04-01 17:05:56 -0400

    Update Signup.java

--- Signup.java
+++ Signup.java
@@ -1,9 +1,9 @@
 
 /*  Project: Vehicle Vortex
-*   Class: Login.java 
+*   Class: Signup.java 
 *   Author: Summer Snyder, Antonios Takos, Teuta Elezaj, Christian Felix, Tahir Buksh, Jayden Kuprel
 *   Date: February 19th, 2023 
-*   This program creates a login page, where users are able to enter their credentials,
+*   This program creates a signup page, where users are able to enter their credentials,
 *   where they will gain access to Vehicle Vortex.
 */ 
 
@@ -12,8 +12,6 @@ import java.awt.*;
 import java.awt.event.*;
 import java.lang.Exception;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.awt.event.ActionListener;
 import java.io.*;
 
@@ -21,124 +19,127 @@ class CreateSignupForm extends JFrame implements ActionListener {
 // Initializing Java Swing Variables
     JButton submit;
     JPanel signupPanel;
-    JLabel signupLabel, userLabel, passwordLabel;
+    JLabel signupLabel, userLabel, passwordLabel, infoLabel;
     final JTextField userField, passwordField;
 
 // ---------------------------------------------------------------------------------
-// This method creates the GUI for the Login form
-    CreateSignupForm() {
-
+// This method creates the GUI for the sign up form
+CreateSignupForm() {
     // Assigning JSwing variables values & styling
 
-        // Login Label
-        signupLabel = new JLabel();
-        signupLabel.setText("Signup Form");
-        signupLabel.setBounds(115, 30, 100, 40);
-        signupLabel.setForeground(Color.WHITE);
-        signupLabel.setFont(new Font("Inter", Font.BOLD, 30));
+    // sign up Label
+    signupLabel = new JLabel();
+    signupLabel.setText("Sign up");
+    signupLabel.setForeground(Color.WHITE);
+    signupLabel.setFont(new Font("Inter", Font.BOLD, 30));
+    Dimension size = signupLabel.getPreferredSize();
+    signupLabel.setBounds(115, 30, size.width, size.height);
 
-        // User Label
-        userLabel = new JLabel();
-        userLabel.setText("Username");
-        userLabel.setBounds(44, 100, 100, 16);
-        userLabel.setForeground(Color.WHITE);
-        userLabel.setFont(new Font("Inter", Font.BOLD, 16));
+    // Info Label
+    infoLabel = new JLabel();
+    infoLabel.setText("<html><center>Thanks for choosing Vehicle Vortex! To sign up, please enter your desired username and password. Each field must be at least 5 characters long, and your password must contain at least one special character.</center></html>");
+    infoLabel.setBounds(20, 80, 320, 60);
+    infoLabel.setForeground(Color.WHITE);
+    infoLabel.setFont(new Font("Inter", Font.PLAIN, 12));
 
-        // User Field
-        userField = new JTextField(15);
-        userField.setBounds(37, 128, 249, 38);
-        userField.setBackground(new Color(217, 217, 217));
-        userField.setBorder(BorderFactory.createCompoundBorder(
-            BorderFactory.createLineBorder(new Color(86, 53, 158)),
-            BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+    // User Label
+    userLabel = new JLabel();
+    userLabel.setText("Username");
+    userLabel.setBounds(44, 170, 100, 16);
+    userLabel.setForeground(Color.WHITE);
+    userLabel.setFont(new Font("Inter", Font.BOLD, 16));
 
-        // Password Label
-        passwordLabel = new JLabel();
-        passwordLabel.setText("Password");
-        passwordLabel.setBounds(44, 170, 100, 16);
-        passwordLabel.setForeground(Color.WHITE);
-        passwordLabel.setFont(new Font("Inter", Font.BOLD, 16));
+    // User Field
+    userField = new JTextField(15);
+    userField.setBounds(37, 198, 249, 38);
+    userField.setBackground(new Color(217, 217, 217));
+    userField.setBorder(BorderFactory.createCompoundBorder(
+        BorderFactory.createLineBorder(new Color(86, 53, 158)),
+        BorderFactory.createEmptyBorder(10, 10, 10, 10)));
 
-        // Password Field
-        passwordField = new JPasswordField(15);
-        passwordField.setBounds(37, 198, 249, 38);
-        passwordField.setBackground(new Color(217, 217, 217));
-        passwordField.setBorder(BorderFactory.createCompoundBorder(
-            BorderFactory.createLineBorder(new Color(86, 53, 158)),
-            BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+    // Password Label
+    passwordLabel = new JLabel();
+    passwordLabel.setText("Password");
+    passwordLabel.setBounds(44, 250, 100, 16);
+    passwordLabel.setForeground(Color.WHITE);
+    passwordLabel.setFont(new Font("Inter", Font.BOLD, 16));
 
-        // Submit
-        submit = new JButton("Submit");
-        submit.setBounds(110, 270, 100, 34);
-        submit.setBackground(new Color(217, 217, 217));
-        submit.setForeground(new Color(86, 53, 158));
-        submit.setFont(new Font("Inter", Font.BOLD, 16));
-        
+    // Password Field
+    passwordField = new JPasswordField(15);
+    passwordField.setBounds(37, 278, 249, 38);
+    passwordField.setBackground(new Color(217, 217, 217));
+    passwordField.setBorder(BorderFactory.createCompoundBorder(
+        BorderFactory.createLineBorder(new Color(86, 53, 158)),
+        BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+
+    // Submit
+    submit = new JButton("Submit");
+    submit.setBounds(110, 350, 100, 34);
+    submit.setBackground(new Color(217, 217, 217));
+    submit.setForeground(new Color(86, 53, 158));
+    submit.setFont(new Font("Inter", Font.BOLD, 16));
 
     // Creating a new Panel
-        signupPanel = new JPanel();
-        signupPanel.setBackground(new Color(86, 53, 158));
-        signupPanel.setLayout(null);
+    JPanel signupPanel = new JPanel();
+    // signupPanel.setPreferredSize(new Dimension(700, 700));
+    signupPanel.setBackground(new Color(86, 53, 158));
+    signupPanel.setLayout(null);
 
-    // Adding variables to 
-        signupPanel.add(signupLabel);
-        signupPanel.add(userLabel);
-        signupPanel.add(userField);
-        signupPanel.add(passwordLabel);
-        signupPanel.add(passwordField);
-        signupPanel.add(submit);
-        add(signupPanel, BorderLayout.CENTER);
+    // Adding variables to
+    signupPanel.add(signupLabel);
+    signupPanel.add(infoLabel);
+    signupPanel.add(userLabel);
+    signupPanel.add(userField);
+    signupPanel.add(passwordLabel);
+    signupPanel.add(passwordField);
+    signupPanel.add(submit);
+    add(signupPanel, BorderLayout.CENTER);
 
     // Creating action listener for the submit button
-        submit.addActionListener(this);
-        setTitle("Signup Form");
-
-    } // <--- CreateSignupForm() constructor ends here
-
+    submit.addActionListener(this);
+    setTitle("Sign up Form");
+    // setPreferredSize(new Dimension(800, 200));
+    signupPanel.setPreferredSize(new Dimension(600, 700));
+    pack();
+}
 
 // ---------------------------------------------------------------------------------
 // Action listener method for the submit button
-public void actionPerformed(ActionEvent ae) {
-    // Assigning the information that will be inputted by the user as string variables
-    String userValue = userField.getText();
-    String passValue = passwordField.getText();
+    public void actionPerformed(ActionEvent ae) {
+        // Assigning the information that will be inputted by the user as string variables
+        String userValue = userField.getText();
+        String passValue = passwordField.getText();
 
-    // Read the existing credentials from the file
-    List<String> credentials = new ArrayList<>();
-    try {
-        BufferedReader reader = new BufferedReader(new FileReader("Credentials.txt"));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            credentials.add(line);
+        // validating username and password based on rules - username must be greater than 5, and password must be at least 5 characters and contain one special character.
+        if (userValue.length() <= 5) {
+            System.out.println("ERROR: Username must be greater than 5 characters long.");
+            return;
         }
-        reader.close();
-    } catch (IOException e) {
-        System.out.println("Error reading credentials from file.");
-    }
-
-    // Check if the inputted credentials match any of the existing credentials
-    boolean found = false;
-    for (String credential : credentials) {
-        String[] parts = credential.split("\\|");
-        String username = parts[0].trim().substring(10);
-        String password = parts[1].trim().substring(10);
-        if (username.equals(userValue) && password.equals(passValue)) {
-            found = true;
-            break;
+        if (!passValue.matches("^(?=.*[!@#$%^&*(),.?\":{}|<>]).{5,}$")) {
+            System.out.println("ERROR: Password must be at least 5 characters long and contain at least one special character.");
+            return;
         }
-    }
 
-    if (found) {
-        // Show a success message if credentials are valid
-        JOptionPane.showMessageDialog(this, "Login successful!", "Success", JOptionPane.INFORMATION_MESSAGE);
-    } else {
-        // Display an error message if credentials are invalid
-        JOptionPane.showMessageDialog(this, "Invalid username or password.", "Error", JOptionPane.ERROR_MESSAGE);
-    }
-}
-}
+        // Write the user-provided credentials to a file
+        try {
+            FileWriter writer = new FileWriter("Credentials.txt", true);
+            LocalDateTime timestamp = LocalDateTime.now();
+            
+            writer.write("Username: " + userValue + " | Password: " + passValue + " | Timestamp: " + timestamp.toString() + System.lineSeparator()); // add newline character
+            writer.close();
+            System.out.println("Credentials successfully saved to file!");
 
+            // Show the option page if credentials are in line with rules
+            OptionPage page = new OptionPage();
+            page.setVisible(true);
 
+            // dispose of sign up page once credentials are valid
+            dispose();
+        } catch (IOException e) {
+            System.out.println("Error writing credentials to file.");
+        }
+    } // <--- actionPerformed() method ends here
+} // <--- CreateSignupForm{} class ends here
 
 class Signup {
     public static void main(String[] args) {
@@ -153,6 +154,6 @@ class Signup {
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
     } // <--- main() method ends here
-} // <--- Login{} class ends here
+} // <--- Signup{} class ends here
 
 
